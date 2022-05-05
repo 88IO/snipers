12ea20edc0c598b4866f6128f9c24314852962ab
88IO <8810aoba@gmail.com>
2022-05-05 21:33:52 +0900

    [Fix] Job構造体の初期化関数のdead_codeを許可

--- src/job.rs
+++ src/job.rs
@@ -24,26 +24,10 @@ pub struct Job {
     pub utc_offset: i32
 }
 
-/*
-impl Hash for Job {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.user_id.hash(state);
-        self.guild_id.hash(state);
-        self.event_type.hash(state);
-    }
-}
-impl PartialEq for Job {
-    fn eq(&self, other: &Self) -> bool {
-        self.user_id == other.user_id
-            && self.guild_id == other.guild_id
-            && self.event_type == other.event_type
-    }
-}
-impl Eq for Job {}
-*/
-
 impl Job {
-    pub fn new(naive_utc: NaiveDateTime, user_id: UserId, guild_id: GuildId, event_type: EventType, utc_offset: i32) -> Self {
+    #[allow(dead_code)]
+    pub fn new(naive_utc: NaiveDateTime, user_id: UserId,
+               guild_id: GuildId, event_type: EventType, utc_offset: i32) -> Self {
         Job { naive_utc, user_id: user_id.0 as i64, guild_id: guild_id.0 as i64, event_type, utc_offset }
     }
 
